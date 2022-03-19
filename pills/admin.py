e3d54199e00314f689b611464bb7c0c2330c6258
Stypox <stypox@pm.me>
2022-03-19 16:21:41 +0100

    Only use pills

--- pills/admin.py
+++ pills/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from pills.models import Pill, PillQuestion, PillAnswer
+from pills.models import Pill
 
 admin.site.register(Pill)
-admin.site.register(PillQuestion)
-admin.site.register(PillAnswer)
--- pills/models.py
+++ pills/models.py
@@ -5,19 +5,3 @@ class Pill(models.Model):
 
     def __str__(self):
         return self.text
-
-class PillQuestion(models.Model):
-    pill = models.ForeignKey(Pill, on_delete=models.CASCADE)
-    question = models.CharField(max_length=200)
-    is_multiple_choice = models.BooleanField()
-
-    def __str__(self):
-        return self.question + " - " + ("multiple choice" if self.is_multiple_choice else "single choice")
-
-class PillAnswer(models.Model):
-    pill_question = models.ForeignKey(PillQuestion, on_delete=models.CASCADE)
-    answer = models.CharField(max_length=200)
-    is_correct = models.BooleanField()
-
-    def __str__(self):
-        return self.answer + " - " + ("correct" if self.is_correct else "incorrect")
