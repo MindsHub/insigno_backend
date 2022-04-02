def008c91b2128351afeba358d0502bf08fd1a6e
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2022-04-02 15:58:11 +0200

    sistemato map

--- map/views.py
+++ map/views.py
@@ -1,10 +1,4 @@
 from django.http import JsonResponse
-from .models import Pill
 import random
 
-def randomPill(request):
-    pks = Pill.objects.values_list('pk', flat=True)
-    random_pk = random.choice(pks)
-    return JsonResponse({
-        'text': Pill.objects.get(pk=random_pk).text
-    })
+
