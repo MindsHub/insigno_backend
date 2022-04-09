f959588752538c63e762a04c0291b6651d98e470
Stypox <stypox@pm.me>
2022-04-09 17:16:44 +0200

    Add REST authentication

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views as authtoken_views
 
 urlpatterns = [
     path('pills/', include('pills.urls')),
     path('admin/', admin.site.urls),
+    path('auth-token/', authtoken_views.ObtainAuthToken.as_view(), name='auth-token'),
 ]
