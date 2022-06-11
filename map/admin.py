047d3592967345d73616b2bd7db23d166078d63c
Alessio <alessio@zeni.info>
2022-06-11 16:54:22 +0200

    marker

--- map/admin.py
+++ map/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from map.models import Marker
+
+# Register your models here.
+admin.site.register(Marker)
--- map/models.py
+++ map/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class Marker(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
 
+    class TypeClass(models.TextChoices):
+        PLASTIC = 'Plastic'
+        PAPER = 'Paper'
+        UNDIFFERENTIATED = 'Undifferentiated'
+        GLASS = "Glass"
+        COMPOST = "Compost"
+        ELECTRONICS = "Electronics"
+
+    type = models.CharField(
+        max_length=20,
+        choices=TypeClass.choices,
+        default=TypeClass.UNDIFFERENTIATED,
+    )
+
+
+    def __str__(self):
+        return f"x =\"{self.x}\" y = {self.y}"
