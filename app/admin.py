6ff27742fc2be8ab7046398ef39b92ac1aa59246
Alessio <alessio@zeni.info>
2022-12-17 14:02:26 +0100

    restarting with rusttt

--- app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from app.models import InsignioUser
-
-admin.site.register(InsignioUser)
--- map/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.gis import admin
-
-from map.models import Marker, MarkerImage
-
-# Register your models here.
-admin.site.register(Marker, admin.GISModelAdmin)
-admin.site.register(MarkerImage)
