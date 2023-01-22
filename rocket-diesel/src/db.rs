962c3d701359ddafd1137927bb1211e2a649f7f1
Alessio <alessio@zeni.info>
2023-01-22 20:50:13 +0100

    now it works

---
+++ rocket-diesel/src/db.rs
@@ -0,0 +1,11 @@
+use rocket::fairing::AdHoc;
+use rocket_sync_db_pools::database;
+
+#[database("db")]
+pub struct Db(diesel::PgConnection);
+
+pub fn stage() -> AdHoc {
+    AdHoc::on_ignite("Diesel PostGres Stage", |rocket| async {
+        rocket.attach(Db::fairing())
+    })
+}
