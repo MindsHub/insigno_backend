c4b4b07e015450f052bd044b7efa71938eceaf0c
Alessio <alessio@zeni.info>
2023-05-22 11:32:49 +0200

    comments

--- src/db.rs
+++ src/db.rs
@@ -2,9 +2,11 @@ use rocket::fairing::AdHoc;
 
 use rocket_sync_db_pools::{database, diesel::PgConnection};
 
+/// our connection pool
 #[database("db")]
 pub struct Db(pub PgConnection);
 
+///init our connection
 pub fn stage() -> AdHoc {
     AdHoc::on_ignite("Diesel PostGres Stage", |rocket| async {
         rocket.attach(Db::fairing())
