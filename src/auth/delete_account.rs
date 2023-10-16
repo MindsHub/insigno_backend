6aca109ec5c2446150e1eedf25b084c186da6c4e
AlessioZeni <alessio.zeni01@gmail.com>
2023-10-16 12:14:22 +0200

    no mail while deleting account

--- src/auth/delete_account.rs
+++ src/auth/delete_account.rs
@@ -1,23 +1,29 @@
-use rocket::http::ContentType;
+use rocket::{http::ContentType, State};
 
 use crate::{
     auth::user::{Authenticated, User},
     db::Db,
     pending::{generate_token, Pending, PendingAction},
     utils::InsignoError,
-    InsignoConfig,
+    InsignoConfig, mail::MailBuilder,
 };
 
 use super::{scrypt::scrypt_simple, user::UserDiesel};
 
 #[post("/delete_account")]
 pub async fn delete_account(
+    mailer: &State<MailBuilder>,
     user: Result<User<Authenticated>, InsignoError>,
     connection: Db,
 ) -> Result<(), InsignoError> {
     let user = user?;
     let mut pend = Pending::new(PendingAction::DeleteUser(user.id.unwrap()));
     pend.insert(&connection).await?;
+    let link = format!("https://insigno.mindshub.it/verify/{}", pend.token);
+    mailer
+        .send_delete_account_mail(&user.email, &user.name, &link)
+        .await
+        .map_err(|e| InsignoError::new(500).debug(e))?;
     Ok(())
 }
 
@@ -32,6 +38,7 @@ pub async fn complete_delete(
         let mut user = UserDiesel::get_by_id(connection, user_id).await?;
         user.email = scrypt_simple(&user.email, &params)
             .or(Err(InsignoError::new(500).debug("error in scrypt")))?;
+
         user.password = "".to_string();
         user.name = format!("ANON_{}", generate_token());
         user.update(connection).await?;
